3867d00adbe758fa552580eaa32b7b9829dbba1c
gaohang <isaackao@foxmail.com>
2018-10-16 09:33:38 +0800

    add eraser&brush button

--- main.js
+++ main.js
@@ -1,19 +1,71 @@
 var pad = document.getElementById('pad')
 var ctx = pad.getContext('2d')
+var actions = document.getElementById('actions')
 
-// 全屏处理
-assignSize()
+autoSetCanvasSize(pad)
 
-// 监听窗口大小变化
-window.onresize = (e) => {
-  assignSize()
+listenToMouse(pad)
+
+// 使用橡皮擦
+var eraserEnable = false
+eraser.addEventListener('click', () => {
+  eraserEnable = true
+  actions.className = "actions on"
+})
+brush.addEventListener('click', () => {
+  eraserEnable = false
+  actions.className = "actions"
+})
+
+function listenToMouse(canvas) {
+  // 模式切换
+  var usingOrNot = false
+  // 记录上一个点
+  var lastPoint = {x: undefined, y: undefined}
+
+  canvas.onmousedown = (e) => {
+    // 绘画模式激活
+    usingOrNot = true
+    var clientX = e.clientX  
+    var clientY = e.clientY
+
+    if(eraserEnable) {
+      ctx.clearRect(clientX - 5, clientY - 5, 10, 10)
+    } else {
+      lastPoint = {x: clientX, y: clientY}
+    }
+  }
+  canvas.onmousemove = (e) => {
+    // 判断绘画模式已开启
+      var clientX = e.clientX
+      var clientY = e.clientY
+      if(!usingOrNot) {return}
+      if(eraserEnable) {
+          ctx.clearRect(clientX - 5, clientY - 5, 10, 10)
+        } else {
+        var newPoint = {x: clientX, y: clientY}
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+  canvas.onmouseup = (e) => {
+    usingOrNot = false
+    }
+  }
 }
+
+function autoSetCanvasSize(canvas) {
+  assignSize()
+
+  window.onresize = (e) => {
+    assignSize()
+  }
 
 function assignSize() {
   var pageHeight = document.documentElement.clientHeight
   var pageWidth = document.documentElement.clientWidth
-  pad.height = pageHeight
-  pad.width = pageWidth
+  canvas.height = pageHeight
+  canvas.width = pageWidth
+  }
 }
 
 // 画线
@@ -25,49 +77,3 @@ function drawLine(x1, y1, x2, y2) {
   ctx.stroke();
   ctx.closePath();
 }
-
-// 模式切换
-var usingOrNot = false
-var eraserEnable = false
-// 记录上一个点
-var lastPoint = {x: undefined, y: undefined}
-
-pad.onmousedown = (e) => {
-  // 绘画模式激活
-  usingOrNot = true
-  var clientX = e.clientX  
-  var clientY = e.clientY
-  if(eraserEnable) {
-    usingOrNot = true
-    ctx.clearRect(clientX - 5, clientY - 5, 10, 10)
-  } else {
-    usingOrNot = true
-    lastPoint = {x: clientX, y: clientY}
-  } 
-}
-
-pad.onmousemove = (e) => {
-  // 判断绘画模式已开启
-    var clientX = e.clientX
-    var clientY = e.clientY
-    if(eraserEnable) {
-      if(usingOrNot) {
-        ctx.clearRect(clientX - 5, clientY - 5, 10, 10)
-      }
-    } else {
-      if(usingOrNot) {
-        var newPoint = {x: clientX, y: clientY}
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-        lastPoint = newPoint
-      }
-    }
-}
-
-pad.onmouseup = (e) => {
-  usingOrNot = false
-}
-
-// 使用橡皮擦
-eraser.addEventListener('click', () => {
-  eraserEnable = true
-})
